7e8019eb439aefdcec7ffeee49160912a2e085ee
AbhishekDas1998 <abhishekdas1998@gmail.com>
2020-01-02 15:10:04 +0530

    After implementing Eureka Server

--- src/main/java/com/spring/EmployeeApplication.java
+++ src/main/java/com/spring/EmployeeApplication.java
@@ -2,12 +2,16 @@ package com.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2 
 @SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
